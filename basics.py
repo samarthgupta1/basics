96de875d77583ba435949158192367ea9b9b65d2
samarthgupta1 <samarth1250@gmail.com>
2018-05-29 11:10:24 -0700

    new python file created

---
+++ basics.py
@@ -0,0 +1,21 @@
+# question 1
+print "hello"
+# question 2
+print"hello"+"world"
+# question 3
+s=input("enter a 1no")
+d=input("enter a 2no")
+a=input("enter a 3no")
+print s,a,d
+# question 4
+print "Lets Get Started"
+# question 5
+s="acadview"
+course="python"
+fees=5000
+e="%s %s %s"
+print e%(s,course,fees)
+# question 7
+name="tony stark "
+salary=1000000
+print('%s''%d')%(name,salary)
